ed7047ce9dd09aa24fdf7f31cb9bf26f893e8f77
iqraAsif <iqrahashmi208@gmail.com>
2019-08-21 13:30:17 +0800

    redirect to user_profile

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,13 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!
+
     def new
     end
     def create
-        @post = Post.new(post_params)
+        @post = Post.new(post_params.merge(user_id: current_user.id))
         
         @post.save
-        redirect_to @post
+        redirect_to "/user_profile"
     end
     def index
         @posts = Post.all
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'landing/index'
   resources :posts
   root 'landing#index'
+  get '/user_profile', to: 'posts#user_profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
