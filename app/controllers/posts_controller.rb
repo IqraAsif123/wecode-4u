491859ec56e809c3b232fd07ad376053e47a80af
iqraAsif <iqrahashmi208@gmail.com>
2019-08-21 17:22:40 +0800

    add logout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,22 @@ class PostsController < ApplicationController
     before_action :authenticate_user!
 
     def new
+      @post = Post.new
     end
     def create
         @post = Post.new(post_params.merge(user_id: current_user.id))
         
-        @post.save
+        if @post.save
         redirect_to "/user_profile"
+        else 
+          render 'new'
+        end
     end
     def index
         @posts = Post.all
     end
     def user_profile
+      @current_user_posts = Post.where(user_id: current_user.id)
     end
     private
   def post_params
