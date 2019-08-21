136dfd2db3daea0f613403a9a3db7dcc8c594818
iqraAsif <iqrahashmi208@gmail.com>
2019-08-21 12:27:34 +0800

    listed post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,17 @@ class PostsController < ApplicationController
     def new
     end
     def create
-        render plain: params[:post].inspe
+        @post = Post.new(post_params)
+        
+        @post.save
+        redirect_to @post
     end
+    def index
+        @posts = Post.all
+    end
+    private
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+  
 end
